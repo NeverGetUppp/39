1396b9ae7d6a8cecb7031ddea1c8e63593509186
NeverGetUppp <128945799+NeverGetUppp@users.noreply.github.com>
2023-03-27 22:38:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Anie lỏ",
-    introTitle: 'Anie',
+    titleWeb: "6E>",
+    introTitle: 'Hằng',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Anie cứ đi nhầm đường
-    Lạc vào tim NEET lẻ loi
+    Mà sao Hằng cứ đi nhầm đường
+    Lạc vào tim V lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng Anie đã yêu NEET ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng Anie đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng Hằng đã yêu V ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng Hằng đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 8 tỉ người mà sao Anie lại yêu NEET <3',
-    btnReply: 'Gửi cho NEET <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'NEET biết mà 🥰. Yêu Anie nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 9h, NEET qua nhà Anie chơi nha. 🥵',
-    btnAccept: 'Dạ ck iu :3 NEET muốn chơi gì cũng được :3 chơi đến sáng lun nha hihi :3💦💦',
-    messLink: 'https://www.facebook.com/messages/t/100009934027755' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    question: 'Trên thế giới hơn 8 tỉ người mà sao Hằng lại yêu V <3',
+    btnReply: 'Gửi cho V <3',
+    reply: 'Em làm bạn anh hơi bị lâu rồi đó. Từ nay về sau em sẽ không làm bạn anh nữa, em sẽ là mái ấm của anh.',
+    mess: 'V biết mà 🥰. Yêu Hằng nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 9h, Hằng qua nhà V chơi nha. 🥵',
+    btnAccept: 'Dạ vk iu :3 Hằng muốn chơi gì cũng được hihi :3💦💦',
+    messLink: 'https://www.facebook.com/messages/t/100009025030668' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
